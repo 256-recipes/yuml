80370ed34742152e56ae6a732e967f8ca640900e
madhead <siarhei.krukau@gmail.com>
2022-01-20 23:41:12 +0100

    Switch to Yarn

--- jest.config.ts
+++ jest.config.ts
@@ -3,5 +3,13 @@ import type { Config } from '@jest/types';
 const config: Config.InitialOptions = {
   testEnvironment: 'node',
   preset: 'ts-jest',
+  globals: {
+    'ts-jest': {
+      diagnostics: {
+        ignoreCodes: ['TS151001'],
+      },
+    },
+  },
 };
+
 export default config;
